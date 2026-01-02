51e6391a04fe4fcc754c93d4d4154726fa5a043f
Engine Eternal <51864059+MaxBotCoder@users.noreply.github.com>
2026-01-02 12:43:35 -0800

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -3,7 +3,7 @@ su
 
 #moving executable to /usr/bin
 mv exigence-executable-installer.py /usr/bin
-mv exigence-executable-uninstaller.sh /usr/bin
 
 #moving desktop shortcut to /usr/share/applications
 mv exigence-executable-installer.desktop /usr/share/applications
+
