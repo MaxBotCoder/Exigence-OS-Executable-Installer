cbedc89ff40f10cdfea0017343b79c1f7b60cf5c
Solomon <51864059+MaxBotCoder@users.noreply.github.com>
2026-01-03 14:55:37 -0800

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,9 +1,7 @@
-#gains root privaledges
-su
-
 #moving executable to /usr/bin
 mv exigence-executable-installer.py /usr/bin
 
 #moving desktop shortcut to /usr/share/applications
 mv exigence-executable-installer.desktop /usr/share/applications
+
 
